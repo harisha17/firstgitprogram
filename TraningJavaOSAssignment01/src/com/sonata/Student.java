f09ec557b3d9d09f5809dc995fe5734855ad6655
harisha17 <94522310+harisha17@users.noreply.github.com>
2021-12-10 10:47:29 +0530

    Update Student.java

--- TraningJavaOSAssignment01/src/com/sonata/Student.java
+++ TraningJavaOSAssignment01/src/com/sonata/Student.java
@@ -25,10 +25,10 @@ class Student {
    public static void main(String[] args) {
       List<Student> studentlist = new ArrayList<Student>();
       studentlist.add(new Student("Harisha", 95)); 
-      studentlist.add(new Student("Ashok", 63)); 
+      studentlist.add(new Student("Ashok", 85)); 
       studentlist.add(new Student("Arjun", 85)); 
-      studentlist.add(new Student("Jaya", 10)); 
-      studentlist.add(new Student("Iswarya", 70));
+      studentlist.add(new Student("Jaya", 80)); 
+      studentlist.add(new Student("Iswarya", 80));
       studentlist.sort((Student s1, Student s2)->s2.getMarks()-s1.getMarks());
       studentlist.forEach((s)->System.out.println(s)); 
    }
